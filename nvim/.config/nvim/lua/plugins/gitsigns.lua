03ae6c4969815e4b9c2fcc386861a23aaee5cfb4
Thomas Hoggard <thoggard@league.com>
2022-03-31 14:44:42 -0400

    feat(nvim): update gitsigns config

--- nvim/.config/nvim/lua/plugins/gitsigns.lua
+++ nvim/.config/nvim/lua/plugins/gitsigns.lua
@@ -2,12 +2,12 @@ require('gitsigns').setup({
   current_line_blame = true, 
   current_line_blame_opts = {
     virt_text = true,
-    virt_text_pos = 'right_align', 
+    virt_text_pos = 'eol', 
     delay = 250,
     ignore_whitespace = false,
   },
 	current_line_blame_formatter_opts = {
     relative_time = true
   },
-  current_line_blame_formatter = '<author>, <author_time> • <summary>',
+  current_line_blame_formatter = '      <author>, <author_time> • <summary>',
 })

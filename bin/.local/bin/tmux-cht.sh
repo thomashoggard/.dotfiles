de71d59c2a68776c02c118204e5c904e81f457fe
Thomas Hoggard <mail@thomashoggard.com>
2022-04-26 08:08:13 -0400

    Format and move some bash scripts

--- bin/.local/bin/tmux-cht.sh
+++ bin/.local/bin/tmux-cht.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-commands | fzf`
+selected=$(cat ~/.tmux-cht-languages ~/.tmux-cht-commands | fzf)
 
 if [[ -z $selected ]]; then
-    exit 0
+	exit 0
 fi
 
 read -p "Enter Query: " query
 
 if grep -qs "$selected" ~/.tmux-cht-languages; then
-    query=`echo $query | tr ' ' '+'`
-    # tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
-    tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
+	query=$(echo $query | tr ' ' '+')
+	# tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+	tmux neww bash -c "curl -s cht.sh/$selected/$query | less"
 else
-    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+	tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
 fi

f6d91ec0503f2a18c010ab93304a81ba3f6cef89
Thomas Hoggard <mail@thomashoggard.com>
2022-07-05 08:27:16 -0400

    feat(nvim): Setup mapping between neogit and gitcommit snippets

--- nvim/.config/nvim/lua/plugins/luasnip.lua
+++ nvim/.config/nvim/lua/plugins/luasnip.lua
@@ -1,11 +1,12 @@
 local ls = require("luasnip")
 
-require("luasnip.loaders.from_lua").load({ paths = "~/.config/nvim/snippets" }) -- Load custom snippets
+require("luasnip.loaders.from_lua").lazy_load({ paths = "~/.config/nvim/snippets" }) -- Load custom snippets
 
 vim.cmd([[command! LuaSnipEdit :lua require("luasnip.loaders.from_lua").edit_snippet_files()]])
 
 vim.keymap.set("n", "<Leader><Leader>s", "<cmd>LuaSnipEdit<cr>", { silent = true, noremap = true })
 
--- ls.config.set_config({
---   -- asdfasdf
+-- ls.config.setup({
 -- })
+
+ls.filetype_extend("NeogitCommitMessage", { "gitcommit" })

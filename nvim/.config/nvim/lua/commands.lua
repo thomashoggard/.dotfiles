5f429d6c14e5c89f83971d02b05cd3b6fb26dfb3
Thomas Hoggard <mail@thomashoggard.com>
2022-07-26 08:30:03 -0400

    feat(nvim): Add autowindow resize autocmd

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -1,2 +1,9 @@
--- Open :h documentation in vertical buffer
-vim.cmd("autocmd FileType help wincmd L")
+-- Open :h documentation in vertical buffer.
+vim.api.nvim_command("autocmd FileType help wincmd L")
+
+-- Automatically resize windows when screen changes size.
+vim.api.nvim_create_autocmd("VimResized", {
+  callback = function()
+    vim.api.nvim_command('wincmd =')
+  end
+})

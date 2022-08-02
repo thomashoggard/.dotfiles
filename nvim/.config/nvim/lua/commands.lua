fda0ba91abce8ebddadf20eef1e1f6cbf1def978
Thomas Hoggard <mail@thomashoggard.com>
2022-08-02 07:16:00 -0400

    feat(nvim): Add yank on highlight autocommand

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -3,7 +3,19 @@ vim.api.nvim_command("autocmd FileType help wincmd L")
 
 -- Automatically resize windows when screen changes size.
 vim.api.nvim_create_autocmd("VimResized", {
+  group = vim.api.nvim_create_augroup("auto-window-resize", { clear = true }),
   callback = function()
     vim.api.nvim_command('wincmd =')
+  end
+})
+
+-- Highlight yanked text for a brief amount of time.
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("yank-highlight", { clear = true }),
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = (vim.fn['hlexists']('HighlightedyankRegion') > 0 and 'HighlightedyankRegion' or 'IncSearch'),
+      timeout = 300
+    })
   end
 })

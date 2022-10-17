6d561d01a113f26930f281a6b80a6062265e8652
Thomas Hoggard <mail@thomashoggard.com>
2022-10-17 11:27:34 -0400

    feat(nvim): update depreciated config

--- nvim/.config/nvim/lua/lsp/handlers.lua
+++ nvim/.config/nvim/lua/lsp/handlers.lua
@@ -152,6 +152,6 @@ end
 
 -- Add additional capabilities supported by nvim-cmp
 local capabilities = vim.lsp.protocol.make_client_capabilities()
-M.capabilities = require("cmp_nvim_lsp").update_capabilities(capabilities)
+M.capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 
 return M

4d99fecde436b3067823036697fa7f90fa3ccd1c
Thomas Hoggard <mail@thomashoggard.com>
2023-01-17 08:54:34 -0500

    convert colorscheme functions to lua api

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,17 +1,18 @@
----@diagnostic disable-next-line: lowercase-global
-function light()
-  vim.o.background = "light"
-  vim.cmd [[colorscheme gruvbox-material]]
-end
+vim.api.nvim_create_user_command("Light",
+  function()
+    vim.o.background = "light"
+    vim.cmd [[colorscheme gruvbox-material]]
+  end,
+  {}
+)
 
----@diagnostic disable-next-line: lowercase-global
-function dark()
-  vim.o.background = "dark"
-  vim.cmd [[colorscheme catppuccin]]
-end
-
-vim.cmd("command Light silent lua light()")
-vim.cmd("command Dark silent lua dark()")
+vim.api.nvim_create_user_command("Dark",
+  function()
+    vim.o.background = "dark"
+    vim.cmd [[colorscheme catppuccin]]
+  end,
+  {}
+)
 
 return {
   {

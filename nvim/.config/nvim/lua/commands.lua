62cd3952404f23e084195384235def088a6d4496
Thomas Hoggard <mail@thomashoggard.com>
2023-03-30 09:36:49 -0400

    nvim(nvim-tree+packages): auto show nvim tree only when directory

--- nvim/.config/nvim/lua/commands.lua
+++ nvim/.config/nvim/lua/commands.lua
@@ -80,7 +80,18 @@ vim.api.nvim_create_user_command("Bd",
 
 -- Open nvim-tree on startup
 vim.api.nvim_create_autocmd({ "VimEnter" }, {
-  callback = function()
+  callback = function(data)
+    -- buffer is a directory
+    local directory = vim.fn.isdirectory(data.file) == 1
+
+    if not directory then
+      return
+    end
+
+    -- change to the directory
+    vim.cmd.cd(data.file)
+
+    -- open the tree
     require("nvim-tree.api").tree.open()
   end
 })

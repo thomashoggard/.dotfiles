0be6384e1b0750f6f579dcc1ae7ee435818e13fc
Thomas Hoggard <mail@thomashoggard.com>
2023-04-16 23:28:49 -0400

    chore(nvim): update packages

--- nvim/.config/nvim/lua/plugins/nvimtree.lua
+++ nvim/.config/nvim/lua/plugins/nvimtree.lua
@@ -6,14 +6,16 @@ return {
     },
     lazy = false,
     keys = {
-      { "<C-n>", "<cmd>NvimTreeToggle<CR>", desc = "Toggle nvim-tree", silent = true },
-      { "<leader>r", "<cmd>NvimTreeRefresh<CR>", desc = "Refresh nvim-tree", silent = true },
-      { "<leader>n", "<cmd>NvimTreeFindFile<CR>", desc = "Find find", silent = true },
+      { "<C-n>",     "<cmd>NvimTreeToggle<CR>",   desc = "Toggle nvim-tree",  silent = true },
+      { "<leader>r", "<cmd>NvimTreeRefresh<CR>",  desc = "Refresh nvim-tree", silent = true },
+      { "<leader>n", "<cmd>NvimTreeFindFile<CR>", desc = "Find find",         silent = true },
     },
     opts = {
+      renderer = {
+        root_folder_label = false,
+      },
       view = {
         adaptive_size = true,
-        hide_root_folder = true,
       },
     }
   },

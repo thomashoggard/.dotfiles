6fb811b773ab6e186545fb7dfb42716cbd1816bc
Thomas Hoggard <mail@thomashoggard.com>
2023-08-24 16:25:41 -0400

    feat(nvim): enter to select autocompletion

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -101,6 +101,9 @@ return {
       local cmp = require('cmp')
 
       cmp.setup({
+        mapping = {
+          ['<CR>'] = cmp.mapping.confirm({ select = false }),
+        },
         window = {
           completion = cmp.config.window.bordered(),
           documentation = cmp.config.window.bordered(),

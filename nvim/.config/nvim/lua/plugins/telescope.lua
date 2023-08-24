4e7391e9cb6530f231066bc73de5423d3e33e9e0
Thomas Hoggard <mail@thomashoggard.com>
2023-08-24 13:51:13 -0400

    feat(nvim): telescope key maps

--- nvim/.config/nvim/lua/plugins/telescope.lua
+++ nvim/.config/nvim/lua/plugins/telescope.lua
@@ -3,10 +3,20 @@ return {
     "nvim-telescope/telescope.nvim",
     cmd = "Telescope",
     keys = {
-      { "<C-p>",      ":Telescope git_files<cr>",  desc = "", silent = true },
-      { "<leader>fp", ":Telescope find_files<cr>", desc = "", silent = true },
-      { "<leader>fg", ":Telescope live_grep<cr>",  desc = "", silent = true },
-      { "<leader>fb", ":Telescope buffers sort_mru=true ignore_current_buffer=true<cr>", desc = "", silent = true
+      { "<C-p>",      ":Telescope git_files<cr>",            desc = "Find Git files",        silent = true },
+      { "<leader>fp", ":Telescope find_files<cr>",           desc = "Find files",            silent = true },
+      { "<leader>fs", ":Telescope lsp_document_symbols<cr>", desc = "Find document symbols", silent = true },
+      {
+        "<leader>fg",
+        ":lua require('telescope').extensions.live_grep_args.live_grep_args()<cr>",
+        desc = "Live Grep",
+        silent = true
+      },
+      {
+        "<leader>fb",
+        ":Telescope buffers sort_mru=true ignore_current_buffer=true<cr>",
+        desc = "List all buffers in chronological order of last accessed",
+        silent = true
       },
       {
         "<leader>fh",
@@ -21,9 +31,16 @@ return {
         desc = "Search on selected text",
         silent = true
       },
+      {
+        "<leader>fw",
+        ":Telescope grep_string<cr>",
+        desc = "Find word",
+        silent = true
+      }
     },
     dependencies = {
-      "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope-live-grep-args.nvim"
     },
     opts = {
       defaults = {
@@ -47,7 +64,34 @@ return {
     build = "make",
     config = function()
       local telescope = require("telescope")
+      local actions = require("telescope.actions")
+      local lga_actions = require("telescope-live-grep-args.actions")
+
+      telescope.setup({
+        defaults = {
+          mappings = {
+            n = {
+              ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist,
+            },
+            i = {
+              ["<C-w>"] = actions.send_selected_to_qflist + actions.open_qflist
+            }
+          }
+        },
+        extensions = {
+          live_grep_args = {
+            auto_quoting = true,
+            mappings = {
+              i = {
+                ["<C-k>"] = lga_actions.quote_prompt(),
+              }
+            }
+          }
+        }
+      })
+
       telescope.load_extension('fzf')
+      telescope.load_extension("live_grep_args")
     end,
   }
 }

2d2bf1bae8b36ef324d7bec0a2a8a79141f07b5a
Thomas Hoggard <mail@thomashoggard.com>
2023-09-26 11:59:25 -0400

    feat(nvim): update color overrides

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -15,9 +15,9 @@ return {
       },
       color_overrides = {
         mocha = {
-          -- base = "#000000",
-          -- mantle = "#000000",
-          -- crust = "#000000",
+          base = "#070D1C",
+          mantle = "#060B18",
+          crust = "#060B18",
         },
       },
     },

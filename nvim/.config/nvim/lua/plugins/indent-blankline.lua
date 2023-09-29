e0564b79a217a4a1d6a27bbf79e1234d58002705
Thomas Hoggard <mail@thomashoggard.com>
2023-09-29 09:20:10 -0400

    chore(nvim): update packages

--- nvim/.config/nvim/lua/plugins/indent-blankline.lua
+++ nvim/.config/nvim/lua/plugins/indent-blankline.lua
@@ -2,11 +2,19 @@ return {
   {
     -- Add indentation line guides.
     "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    -- opts = {}
     opts = function()
-      vim.cmd([[highlight IndentBlanklineContextChar guifg=#868E96 gui=nocombine]])
+      vim.cmd([[highlight IblIndent guifg=#0D1935 gui=nocombine]])
+      vim.cmd([[highlight IblScope guifg=#192F66 gui=nocombine]])
 
       return {
-        show_current_context = true,
+        indent = {
+          char = '┊'
+        },
+        whitespace = {
+          highlight = { "Whitespace" }
+        },
       }
     end,
   }

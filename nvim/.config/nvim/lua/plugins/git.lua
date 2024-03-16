108a9c3d79da89bca09ce01230c0bc27ecd9e9d5
Thomas Hoggard <mail@thomashoggard.com>
2024-03-16 09:44:44 -0400

    feat(nvim): add diffview

--- nvim/.config/nvim/lua/plugins/git.lua
+++ nvim/.config/nvim/lua/plugins/git.lua
@@ -32,7 +32,8 @@ return {
     "NeogitOrg/neogit",
     cmd = "Neogit",
     dependencies = {
-      "nvim-lua/plenary.nvim"
+      "nvim-lua/plenary.nvim",
+      "sindrets/diffview.nvim",
     },
     keys = {
       { "<leader>gg", ":Neogit<cr>" },

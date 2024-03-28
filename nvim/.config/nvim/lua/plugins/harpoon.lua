19673665bd533289ebfd464e51d3e0916ba86c03
Thomas Hoggard <mail@thomashoggard.com>
2024-03-28 10:43:52 -0400

    feat: autosave harpoon when closing window

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -5,7 +5,11 @@ return {
   config = function()
     local harpoon = require("harpoon")
 
-    harpoon:setup()
+    harpoon:setup({
+      settings = {
+        save_on_toggle = true,
+      },
+    })
 
     vim.keymap.set("n", "<leader>ha", function() harpoon:list():append() end)
     vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)

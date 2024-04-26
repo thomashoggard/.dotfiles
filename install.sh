fd9fc1322e22f4680422c5f59677021fd59d2a80
Thomas Hoggard <mail@thomashoggard.com>
2024-04-26 16:47:44 -0400

    fix stow issue

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 # We stow this folder multiple times. We create it first so the folder itself isn't a symlink.
-# mkdir ${HOME}/.config/kitty
+mkdir ${HOME}/.config/alacritty
 # mkdir ${HOME}/.local/bin
 
 # Stows all folders into ~

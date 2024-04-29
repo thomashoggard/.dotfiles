69a4f48f1e9638ec9e677405845b335e97348861
Thomas Hoggard <mail@thomashoggard.com>
2024-04-29 10:13:34 -0400

    feat(nvim): add key map to clear harpoon list

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -13,6 +13,7 @@ return {
 
     vim.keymap.set("n", "<leader>ha", function() harpoon:list():add() end)
     vim.keymap.set("n", "<leader>hh", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
+    vim.keymap.set("n", "<leader>hc", function() harpoon:list():clear() end)
 
     vim.keymap.set("n", "<leader>j", function() harpoon:list():select(1) end)
     vim.keymap.set("n", "<leader>k", function() harpoon:list():select(2) end)

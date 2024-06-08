62054c43389b1150e5e5641b71bbb0fe8f273647
Thomas Hoggard <mail@thomashoggard.com>
2024-06-08 12:48:11 -0400

    feat(nvim): replace null-ls

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -30,9 +30,6 @@ return {
       -- Snippets
       { 'saadparwaiz1/cmp_luasnip' },
       { 'L3MON4D3/LuaSnip' },
-
-      -- null-ls
-      { "jose-elias-alvarez/null-ls.nvim" },
       { "nvim-lua/plenary.nvim" }
     },
     config = function()
@@ -120,16 +117,6 @@ return {
       })
 
       lsp.setup()
-
-      -- Configure null-ls
-      local null_ls = require('null-ls')
-
-      null_ls.setup({
-        sources = {
-          null_ls.builtins.formatting.prettierd,
-          -- null_ls.builtins.code_actions.eslint_d,
-        }
-      })
 
       -- Configure cmp
       local cmp = require('cmp')

fc5de64cfb76a86eb83e61fac41fc96158b18a05
Thomas Hoggard <mail@thomashoggard.com>
2024-06-08 14:07:51 -0400

    feat: disable which-key

--- nvim/.config/nvim/lua/plugins/which-key.lua
+++ nvim/.config/nvim/lua/plugins/which-key.lua
@@ -1,9 +1,12 @@
-return {
-  "folke/which-key.nvim",
-  event = "VeryLazy",
-  init = function()
-    vim.o.timeout = true
-    vim.o.timeoutlen = 500
-  end,
-  opts = {}
-}
+return {}
+
+-- Messing up LSP keybindings for some reason.
+-- return {
+--   "folke/which-key.nvim",
+--   event = "VeryLazy",
+--   init = function()
+--     vim.o.timeout = true
+--     vim.o.timeoutlen = 500
+--   end,
+--   opts = {}
+-- }

05fb627d4c7f6b46c15944a8875f87f23f29c17c
Thomas Hoggard <mail@thomashoggard.com>
2024-06-08 12:28:52 -0400

    feat(nvim): adjust timeout for which-key and don't move cusor for nvim-surround

--- nvim/.config/nvim/lua/plugins/init.lua
+++ nvim/.config/nvim/lua/plugins/init.lua
@@ -32,8 +32,12 @@ return {
   },
   {
     "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
     config = function()
-      require("nvim-surround").setup()
+      require("nvim-surround").setup({
+        move_cursor = false
+      })
     end
   },
   -- Show linting errors in a panel

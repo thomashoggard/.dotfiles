c104d72b4018a11e2d5d1926548ed75de061bdd8
Thomas Hoggard <mail@thomashoggard.com>
2024-06-14 07:42:04 -0400

    feat(wezterm): adjust config

--- wezterm/.config/wezterm/wezterm.lua
+++ wezterm/.config/wezterm/wezterm.lua
@@ -1,20 +1,23 @@
 local wezterm = require("wezterm")
 local additional_settings = require("additional-settings")
 
-local config = {
-  color_scheme = "Tokyo Night",
-  enable_tab_bar = false,
-  font_size = 14,
-  font = wezterm.font({
-    family = "JetBrains Mono",
-    harfbuzz_features = { "calt=0", "clig=0", "liga=0" },
-  }),
-  window_padding = {
-    left = 0,
-    right = 0,
-    top = 0,
-    bottom = 0,
-  },
+local config = wezterm.config_builder()
+
+config.color_scheme = "Tokyo Night"
+config.font = wezterm.font({
+  family = "JetBrains Mono",
+  weight = "Medium",
+})
+
+config.enable_tab_bar = false
+
+-- config.window_decorations = "RESIZE"
+config.window_close_confirmation = "NeverPrompt"
+config.window_padding = {
+  left = 0,
+  right = 0,
+  top = 0,
+  bottom = 0,
 }
 
 additional_settings.apply_to_config(config)

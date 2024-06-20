6a8e2cf671198bf5fa8007bb7dd086d32e50c2a8
Thomas Hoggard <mail@thomashoggard.com>
2024-06-20 14:31:22 -0400

    feat: trigger linter on TextChange autocmd event

--- nvim/.config/nvim/lua/plugins/linting.lua
+++ nvim/.config/nvim/lua/plugins/linting.lua
@@ -13,7 +13,12 @@ return {
 
     local lint_augroup = vim.api.nvim_create_augroup("lint", { clear = true })
 
-    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
+    vim.api.nvim_create_autocmd({
+      "BufEnter",
+      "BufWritePost",
+      "InsertLeave",
+      "TextChanged", -- text is modified in normal mode.
+    }, {
       group = lint_augroup,
       callback = function()
         lint.try_lint()

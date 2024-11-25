09822a85f6ba10d936c72093f0787047963ce68b
Thomas Hoggard <mail@thomashoggard.com>
2024-11-25 11:07:30 -0500

    feat(nvim): switch to native comments

--- nvim/.config/nvim/lua/plugins/comment.lua
+++ nvim/.config/nvim/lua/plugins/comment.lua
@@ -1,13 +1,7 @@
 return {
   {
-    "numToStr/Comment.nvim",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-    },
-    config = function()
-      require("Comment").setup({
-        pre_hook = require("ts_context_commentstring.integrations.comment_nvim").create_pre_hook(),
-      })
-    end,
+    "folke/ts-comments.nvim",
+    opts = {},
+    event = "VeryLazy",
   },
 }
--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -12,12 +12,6 @@ return {
       require("nvim-treesitter.configs").setup(opts)
     end,
   },
-  {
-    "JoosepAlviste/nvim-ts-context-commentstring",
-    dependencies = {
-      "nvim-treesitter/nvim-treesitter",
-    },
-  },
   -- Automatically add and rename html tags.
   {
     "windwp/nvim-ts-autotag",

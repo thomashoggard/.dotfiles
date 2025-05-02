0ebf788e5ae1f714693e82c420dda2b696af0333
Thomas Hoggard <mail@thomashoggard.com>
2025-05-02 08:54:58 -0400

    refactor(nvim): update diagnostic config to latest standard

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -82,17 +82,19 @@ return {
       })
 
       -- Setup diagnostics.
-      local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-      for type, icon in pairs(signs) do
-        local hl = "DiagnosticSign" .. type
-        vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-      end
-
       vim.diagnostic.config({
         virtual_text = false,
         float = {
           border = "rounded",
           header = "",
+        },
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.INFO] = " ",
+            [vim.diagnostic.severity.HINT] = "󰠠 ",
+          },
         },
       })
     end,

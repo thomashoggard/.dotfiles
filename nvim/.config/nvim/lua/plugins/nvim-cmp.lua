eee61626e1d3f0187f9d32ffed0eac21d45b8d00
Thomas Hoggard <mail@thomashoggard.com>
2025-07-23 21:23:14 -0400

    fix copilot not setup called error

--- nvim/.config/nvim/lua/plugins/nvim-cmp.lua
+++ nvim/.config/nvim/lua/plugins/nvim-cmp.lua
@@ -3,10 +3,16 @@ return {
   lazy = false,
   dependencies = {
     {
+      "zbirenbaum/copilot.lua",
+      config = function()
+        require("copilot").setup({})
+      end,
+    },
+    {
       "zbirenbaum/copilot-cmp",
       dependencies = { "zbirenbaum/copilot.lua" },
       config = function()
-        require("copilot_cmp").setup()
+        require("copilot_cmp").setup({})
       end,
     },
     { "hrsh7th/cmp-buffer" },

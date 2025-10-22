8e253d3c99eb02ba1fe88a470b8073b587d8f219
Thomas Hoggard <mail@thomashoggard.com>
2025-10-22 11:40:46 -0400

    fix: don't include query in branch name

--- fish/.config/fish/functions/git_recent_checkout.fish
+++ fish/.config/fish/functions/git_recent_checkout.fish
@@ -1,5 +1,5 @@
 function git_recent_checkout
-  set branch (git reflog | grep -Eio "moving from ([^[:space:]]+)" | awk '{ print $3 }' | awk ' !x[$0]++' | grep -Ev '^[a-f0-9]{40}$' | fzf --print-query | xargs) 
+  set branch (git reflog | grep -Eio "moving from ([^[:space:]]+)" | awk '{ print $3 }' | awk ' !x[$0]++' | grep -Ev '^[a-f0-9]{40}$' | fzf | xargs) 
 
   if test -z "$branch" 
     echo "Aborting, no branch selected"

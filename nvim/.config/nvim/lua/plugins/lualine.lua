4f14399ccbd8f9835f70c6b233c5f634f7fddce1
Thomas Hoggard <mail@thomashoggard.com>
2026-02-23 08:44:20 -0500

    chore: update repo

--- nvim/.config/nvim/lua/plugins/lualine.lua
+++ nvim/.config/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "hoob3rt/lualine.nvim",
+    "nvim-lualine/lualine.nvim",
     opts = {
       sections = {
         lualine_c = {
--- nvim/.config/nvim/lua/plugins/quickfix.lua
+++ nvim/.config/nvim/lua/plugins/quickfix.lua
@@ -1,4 +1,5 @@
 -- Pretty quickfix list.
+-- NOTE: Unmaintained (last commit Aug 2024). Works fine, revisit if issues arise.
 return {
   "yorickpeterse/nvim-pqf",
   config = function()

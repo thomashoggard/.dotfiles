01ecfbbb9ad3011cbf7e215617a8e5e271f12499
Thomas Hoggard <mail@thomashoggard.com>
2026-04-20 10:54:19 -0400

    chore(nvim): remove unused plugin specs
    Delete dressing.nvim spec (plugin archived, was already disabled).
    Remove commented-out colorscheme entries (nvim-tundra, gruvbox-material).

--- nvim/.config/nvim/lua/plugins/colorscheme.lua
+++ nvim/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,4 @@
 return {
-  -- {
-  --   "sam4llis/nvim-tundra",
-  --   opts = {},
-  --   config = function(_, opts)
-  --     -- vim.g.tundra_biome = "jungle" -- 'arctic' or 'jungle'
-  --     require("nvim-tundra").setup(opts)
-  --     -- vim.cmd.colorscheme("tundra")
-  --   end,
-  -- },
-  -- {
-  --   "tiagovla/tokyodark.nvim",
-  --   opts = {},
-  --   config = function(_, opts)
-  --     require("tokyodark").setup(opts)
-  --     -- vim.cmd.colorscheme("tokyodark")
-  --   end,
-  -- },
   {
     "folke/tokyonight.nvim",
     lazy = false,
@@ -55,25 +38,6 @@ return {
   --     vim.cmd.colorscheme("catppuccin")
   --   end,
   -- },
-  {
-    "sainnhe/gruvbox-material",
-    enabled = true,
-    priority = 1000,
-    config = function()
-      vim.g.gruvbox_material_transparent_background = 1
-      vim.g.gruvbox_material_foreground = "mix"
-      vim.g.gruvbox_material_background = "hard"
-      vim.g.gruvbox_material_ui_contrast = "high"
-      vim.g.gruvbox_material_float_style = "bright"
-      vim.g.gruvbox_material_statusline_style = "material"
-      vim.g.gruvbox_material_cursor = "auto"
-
-      -- vim.g.gruvbox_material_colors_override = { bg0 = '#16181A' } -- #0e1010
-      -- vim.g.gruvbox_material_better_performance = 1
-
-      -- vim.cmd.colorscheme("gruvbox-material")
-    end,
-  },
   {
     "rebelot/kanagawa.nvim",
     lazy = false,
